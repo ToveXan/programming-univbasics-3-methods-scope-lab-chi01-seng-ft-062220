d1fb24352a7555f88492ad34a234037b106c0019
Zoe Xanos <zxanos@gmail.com>
2020-05-08 03:55:20 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,20 +1,20 @@
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
-  return phrase
+  phrase
 end
 
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
-  return status
+  status
 end
 
 def link
   sword = "It's Dangerous To Go Alone! Take This."
   puts sword
-  return sword
+  sword
 end
 
 mario
